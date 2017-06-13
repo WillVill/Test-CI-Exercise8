3b7e52f167e6d685b758bdee9ca00684b217747f
WillVill <bech.william2@gmail.com>
2017-06-13 19:56:47 +0200

    initial

---
+++ target/generated-sources/annotations/exercise/realdatabase/db_original/SystemUser_.java
@@ -0,0 +1,15 @@
+package exercise.realdatabase.db_original;
+
+import exercise.realdatabase.SystemUser;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-13T19:37:43")
+@StaticMetamodel(SystemUser.class)
+public class SystemUser_ { 
+
+    public static volatile SingularAttribute<SystemUser, String> password;
+    public static volatile SingularAttribute<SystemUser, String> userName;
+
+}
